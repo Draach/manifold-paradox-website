326abf6796e5ff758bef62a7941864f2c4b99b47
JulioMissart <juliomissart.mdq@gmail.com>
2023-11-16 03:36:45 -0300

    responsiveness

--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -8,27 +8,8 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 export class LandingComponent {
   @ViewChild('myVideo') myVideo!: ElementRef;
 
-  readyToDisplay: boolean = false;
-
   ngAfterViewInit() {
-    setTimeout(() => {
-      this.readyToDisplay = false;
-      const savedTime = localStorage.getItem('videoCurrentTime');
-      const video: HTMLVideoElement = this.myVideo.nativeElement;
-      if (savedTime) {
-        video.currentTime = parseFloat(savedTime);
-      }
-      video.muted = true;
-      this.readyToDisplay = true;
-    }, 0);
-  }
-
-  onTimeUpdate(): void {
-    let currentTime = this.myVideo.nativeElement.currentTime;
-    currentTime += 1;
-    if (currentTime > this.myVideo.nativeElement.duration) {
-      currentTime = 0;
-    }
-    localStorage.setItem('videoCurrentTime', currentTime.toString());
+    const video: HTMLVideoElement = this.myVideo.nativeElement;
+    video.muted = true;
   }
 }
