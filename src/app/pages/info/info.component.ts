7cc19b9e8f2378a36c68fc5a97129112d9792e54
JulioMissart <juliomissart.mdq@gmail.com>
2023-11-20 19:27:54 -0300

    modified info component to use signals and onpush change detection strategy

--- src/app/pages/info/info.component.ts
+++ src/app/pages/info/info.component.ts
@@ -1,14 +1,15 @@
-import { Component, HostListener } from '@angular/core';
+import { ChangeDetectionStrategy, Component, HostListener, signal } from '@angular/core';
 
 @Component({
   selector: 'app-info',
   standalone: true,
+  changeDetection: ChangeDetectionStrategy.OnPush,
   templateUrl: './info.component.html',
   styleUrls: ['./info.component.scss'],
 
 })
 export default class InfoComponent {
-  imageUrl: string = "https://res.cloudinary.com/daxhopqbg/image/upload/v1700516832/manifold-paradox/Web/info_mobile_spanish_pxb5br.png";
+  imageUrl = signal("https://res.cloudinary.com/daxhopqbg/image/upload/v1700516832/manifold-paradox/Web/info_mobile_spanish_pxb5br.png");
 
   @HostListener('window:resize', ['$event'])
   onResize(event: Event) {
@@ -24,9 +25,9 @@ export default class InfoComponent {
     console.log(`Current width ${viewportWidth}`)
     // Customize this logic based on your specific requirements
     if (viewportWidth < 768) {
-      this.imageUrl = 'https://res.cloudinary.com/daxhopqbg/image/upload/v1700516832/manifold-paradox/Web/info_mobile_spanish_pxb5br.png';
+      this.imageUrl.update(value => 'https://res.cloudinary.com/daxhopqbg/image/upload/v1700516832/manifold-paradox/Web/info_mobile_spanish_pxb5br.png');
     } else {
-      this.imageUrl = 'https://res.cloudinary.com/daxhopqbg/image/upload/v1700516833/manifold-paradox/Web/info_desktop_spanish_njziai.png';
+      this.imageUrl.update(value => 'https://res.cloudinary.com/daxhopqbg/image/upload/v1700516833/manifold-paradox/Web/info_desktop_spanish_njziai.png');
     }
   }
 }
