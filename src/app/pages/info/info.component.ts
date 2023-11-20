15d2c18452e562545fdc94c057a358c52b870811
JulioMissart <juliomissart.mdq@gmail.com>
2023-11-20 19:19:08 -0300

    Implemented info page

--- src/app/pages/info/info.component.ts
+++ src/app/pages/info/info.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-info',
@@ -7,4 +7,26 @@ import { Component } from '@angular/core';
   styleUrls: ['./info.component.scss'],
 
 })
-export default class InfoComponent { }
+export default class InfoComponent {
+  imageUrl: string = "https://res.cloudinary.com/daxhopqbg/image/upload/v1700516832/manifold-paradox/Web/info_mobile_spanish_pxb5br.png";
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: Event) {
+    this.setResponsiveString((event.target as Window).innerWidth);
+  }
+
+  constructor() {
+    // Initialize the string based on the initial viewport width
+    this.setResponsiveString(window.innerWidth);
+  }
+
+  private setResponsiveString(viewportWidth: number): void {
+    console.log(`Current width ${viewportWidth}`)
+    // Customize this logic based on your specific requirements
+    if (viewportWidth < 768) {
+      this.imageUrl = 'https://res.cloudinary.com/daxhopqbg/image/upload/v1700516832/manifold-paradox/Web/info_mobile_spanish_pxb5br.png';
+    } else {
+      this.imageUrl = 'https://res.cloudinary.com/daxhopqbg/image/upload/v1700516833/manifold-paradox/Web/info_desktop_spanish_njziai.png';
+    }
+  }
+}
--- src/app/pages/landing/landing.component.ts
+++ src/app/pages/landing/landing.component.ts
@@ -11,7 +11,6 @@ import { Component, ElementRef, ViewChild } from '@angular/core';
 export default class LandingComponent {
   @ViewChild('myVideo') myVideo!: ElementRef;
   public readonly videoUrl: string = "https://res.cloudinary.com/daxhopqbg/video/upload/v1700105121/manifold-paradox/header_video_niv01i.mp4";
-  public readyToDisplay: boolean = false;
 
   ngAfterViewInit() {
     const video: HTMLVideoElement = this.myVideo.nativeElement;
